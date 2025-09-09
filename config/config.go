3d6a42b4508fba53740f83a405a905f45beb377c
thomzes <thomz161@gmail.com>
2025-09-09 22:34:41 +0700

    feat: create setup config database and env

--- config/config.go
+++ config/config.go
@@ -1,6 +1,46 @@
 package config
 
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/thomzes/user-service-booking-app/common/util"
+)
+
 var Config AppConfig
 
 type AppConfig struct {
+	Port                  int      `json:"port"`
+	AppName               string   `json:"appName"`
+	AppEnv                string   `json:"appEnv"`
+	SignatureKey          string   `json:"signatureKey"`
+	Database              Database `json:"database"`
+	RateLimiterMaxRequest float64  `json:"rateLimiterMaxRequest"`
+	RateLimiterTimeSecond int      `json:"rateLimiterTimeSecond"`
+	JwtSecretKey          string   `json:"jwtSecretKey"`
+	JwtExpirationTime     int      `json:"jwtExpirationTime"`
+}
+
+type Database struct {
+	Host                  string `json:"host"`
+	Port                  int    `json:"port"`
+	Name                  string `json:"name"`
+	Username              string `json:"username"`
+	Password              string `json:"password"`
+	MaxOpenConnection     int    `json:"maxOpenConnection"`
+	MaxLifeTimeConnection int    `json:"maxLifeTimeConnection"`
+	MaxIdleConnection     int    `json:"maxIdleConnection"`
+	MaxIdleTime           int    `json:"maxIdleTime"`
+}
+
+func Init() {
+	err := util.BindFromJSON(&Config, "config.json", ".")
+	if err != nil {
+		logrus.Errorf("failed to bind env from json: %v", err)
+		err = util.BindFromConsul(&Config, os.Getenv("CONSUL_HTTP_URL"), os.Getenv("CONSUL_HTTP_KEY"))
+		if err != nil {
+			panic(err)
+		}
+	}
+
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/go-playground/validator/v10 v10.20.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.21.0
+	gorm.io/driver/postgres v1.6.0
+	gorm.io/gorm v1.30.5
 )
 
 require (
@@ -22,6 +24,12 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -43,9 +51,9 @@ require (
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
