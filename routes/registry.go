f83e681dc74a11902f986d21f20266e12a1a88a1
thomzes <thomz161@gmail.com>
2025-09-10 22:17:44 +0700

    feat: create setup routes

---
+++ routes/registry.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thomzes/user-service-booking-app/controllers"
+	routes "github.com/thomzes/user-service-booking-app/routes/user"
+)
+
+type Registry struct {
+	controller controllers.IControllerRegistry
+	group      *gin.RouterGroup
+}
+
+type IRouteRegister interface {
+	Serve()
+}
+
+func NewRegistry(controller controllers.IControllerRegistry, group *gin.RouterGroup) IRouteRegister {
+	return &Registry{controller: controller, group: group}
+}
+
+func (r *Registry) Serve() {
+	r.userRoute().Run()
+}
+
+func (r *Registry) userRoute() routes.IUserRoute {
+	return routes.NewUserRoute(r.controller, r.group)
+}
---
+++ routes/user/user.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thomzes/user-service-booking-app/controllers"
+	"github.com/thomzes/user-service-booking-app/middlewares"
+)
+
+type UserRoute struct {
+	controller controllers.IControllerRegistry
+	group      *gin.RouterGroup
+}
+
+type IUserRoute interface {
+	Run()
+}
+
+func NewUserRoute(controller controllers.IControllerRegistry, group *gin.RouterGroup) IUserRoute {
+	return &UserRoute{controller: controller, group: group}
+}
+
+func (u *UserRoute) Run() {
+	group := u.group.Group("/auth")
+	group.GET("/user", middlewares.Authenticate(), u.controller.GetUserController().GetUserLogin)
+	group.GET("/:uuid", middlewares.Authenticate(), u.controller.GetUserController().GetUserByUUID)
+
+	group.POST("/login", middlewares.Authenticate(), u.controller.GetUserController().Login)
+	group.POST("/register", middlewares.Authenticate(), u.controller.GetUserController().Register)
+
+	group.PUT("/:uuid", middlewares.Authenticate(), u.controller.GetUserController().Update)
+}
